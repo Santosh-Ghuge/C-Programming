39d659a582c6b3c4f895dd9da4f747fcd470971c
Santosh Ghuge <santoshghuge.sit.comp@gmail.com>
2023-08-27 15:36:52 +0530

    Add files via upload

---
+++ Array1.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+int main()
+{
+    int No1 = 10;
+    int No2 = 20;
+    int No3 = 30;
+    int No4 = 40;
+    int No5 = 50;
+
+    int Arr[5] = {10,20,30,40,50};
+
+    printf("%d\n",No1);
+
+    printf("%d\n", No2);
+
+    printf("%d\n", No3);
+
+    printf("%d\n", No4);
+
+    printf("%d\n", No5);
+
+    printf("%d\n", Arr[0]);
+    
+    printf("%d\n", Arr[1]);
+
+    printf("%d\n",Arr[2]);
+
+    printf("%d\n",Arr[3]);
+    
+    printf("%d\n",Arr[4]);
+
+
+    return 0;
+}
