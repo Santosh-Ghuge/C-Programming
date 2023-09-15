97e63fed06d088ee08a69649a66ab5b7322d9595
Santosh Ghuge <santoshghuge.sit.comp@gmail.com>
2023-09-16 00:48:02 +0530

    Add files via upload

--- Array1.c
+++ Array1.c
@@ -10,26 +10,11 @@ int main()
 
     int Arr[5] = {10,20,30,40,50};
 
-    printf("%d\n",No1);
-
-    printf("%d\n", No2);
-
-    printf("%d\n", No3);
-
-    printf("%d\n", No4);
-
-    printf("%d\n", No5);
-
-    printf("%d\n", Arr[0]);
-    
-    printf("%d\n", Arr[1]);
-
-    printf("%d\n",Arr[2]);
+    printf("%d\n",No4);
 
     printf("%d\n",Arr[3]);
+
+    printf("Size of array is : %d\n",sizeof(Arr));
     
-    printf("%d\n",Arr[4]);
-
-
     return 0;
 }
--- Array2.c
+++ Array2.c
@@ -2,33 +2,20 @@
 
 int main()
 {
-    int iArr[4] = {10,20,30,40};
+    char cArr[4] = {'A','B','C','D'};
 
-    char cArr[4] =  {'A','B','C','D'};
+
+
+    int iArr[4] = {11,21,51,101};
+
+
 
     float fArr[4] = {10.5,20.5,30.5,40.5};
 
-    double dArr[4] = {10.0,20.0,30.0,40.0};
 
-    printf("%d\n",iArr[0]);
-    printf("%d\n",iArr[1]);
-    printf("%d\n",iArr[2]);
-    printf("%d\n",iArr[3]);
 
-    printf("%c\n", cArr[0]);
-    printf("%c\n", cArr[1]);
-    printf("%c\n", cArr[2]);
-    printf("%c\n", cArr[3]);
+    double dArr[4] = {11.0,21.0,51.0,101.0};
 
-    printf("%f\n", fArr[0]);
-    printf("%f\n", fArr[1]);
-    printf("%f\n", fArr[2]);
-    printf("%f\n", fArr[3]);
-
-    printf("%f\n", dArr[0]);
-    printf("%f\n", dArr[1]);
-    printf("%f\n", dArr[2]);
-    printf("%f\n", dArr[3]);
 
     return 0;
 }
